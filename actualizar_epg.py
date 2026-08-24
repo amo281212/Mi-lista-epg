73fa9fac4efcbc656481f54c8ad7f2b4c589f649
amo281212 <digitalpostchile@gmail.com>
2026-08-24 17:40:42 -0400

    Update actualizar_epg.py

--- actualizar_epg.py
+++ actualizar_epg.py
@@ -81,7 +81,6 @@ MIS_CANALES = {
 # 🔄 MAPEO COMPLETO (Recupera AXN y otros canales desde las guías externas)
 MAPEO_IDS = {
     'Sony.co': 'Sony.cl',
-    'Sony.ar': 'Sony.cl',
     'AXN.ar': 'AXN.cl',
     'AXN.co': 'AXN.cl',
     'DiscoveryHomeAndHealth.ar': 'DiscoveryHomeAndHealth.cl',
